c16b84d89f87b0fad1ccbf8514f1ed9e1e91e077
Keila Lopez <keilalofra@gmail.com>
2022-09-28 20:51:28 -0700

    header updates h1 and h2

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+/* 
+const h20 = document.createElement("h20");
+h20.textContent = "This content added by JavaScript";
 
-document.querySelector("body").appendChild(h2);
+document.querySelector("body").appendChild(h20);
+*/
